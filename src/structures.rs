df5e55db7f932b6b0f3ffb46fcd6006d4ab267a9
Andrew <126andrew.zheng@gmail.com>
2025-04-30 00:31:02 -0400

    fixed bug with checking sign for arccos

--- src/structures.rs
+++ src/structures.rs
@@ -1,4 +1,4 @@
-use libm::{acos, cos};
+use libm::{acos, cos, sin};
 use ndarray_stats::DeviationExt;
 use crate::{A1, A2};
 
@@ -10,11 +10,22 @@ pub mod vertex_point;
 
 fn check_if_positive(v: &A1, d: &A1, angle: f64) -> i8 {
     // check sign of acos
+    // println!("----------point: {:?}, v: {:?}", &d, &v);
     let origin = A1::zeros(3);
-    if (v.l2_dist(&origin).unwrap() * cos(acos(d.l2_dist(&origin).unwrap() / v.l2_dist(&origin).unwrap()) + angle) - v[0]).abs() < 1e-10 {
+    let r = v.l2_dist(&origin).unwrap();
+    let temp_angle = acos(d.l2_dist(&origin).unwrap() / v.l2_dist(&origin).unwrap());
+    let positive_point_x = r * cos(angle + temp_angle);
+    let positive_point_y = r * sin(angle + temp_angle);
+    let negative_point_x = r * cos(angle - temp_angle);
+    let negative_point_y = r * sin(angle - temp_angle);
+    let positive_point = A1::from_vec(vec![positive_point_x, positive_point_y, 0.0]);
+    let negative_point = A1::from_vec(vec![negative_point_x, negative_point_y, 0.0]);
+    
+    // println!("+: [{:?}, {:?}], -: [{:?}, {:?}]", positive_point_x, positive_point_y, negative_point_x, negative_point_y);
+    if v.l2_dist(&positive_point).unwrap() < v.l2_dist(&negative_point).unwrap() {
+        // println!("result = positive");
         1
     } else {
         -1
     }
 }
-
